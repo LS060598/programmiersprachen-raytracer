5d314af8e435e5cb71125e5730848b2811270f7d
LS060598 <laura.simon@uni-weimar.de>
2017-06-12 16:31:25 +0200

    Add class Box

---
+++ framework/box.cpp
@@ -0,0 +1,20 @@
+#include "box.hpp"
+#include <cmath>
+#include <catch.hpp>
+#include <algorithm>
+/*
+//destructor
+Box::Box():
+	Shape(),
+	min_{0.0,0.0,0.0},
+    max_{0.0,0.0,0.0},
+
+//getter
+glm::vec3 const& Box::getMax() const {
+    return max_;
+}
+
+glm::vec3 const& Box::getMin() const {
+    return min_;
+}
+*/
---
+++ framework/box.hpp
@@ -0,0 +1,23 @@
+/*
+#ifndef BUW_BOX_HPP
+#define BUW_BOX_HPP
+#include "shape.hpp"
+
+
+class Box : public Shape{
+   
+    public: 
+        glm::vec3 getMax() const;
+        glm::vec3 getMin() const;
+
+
+
+
+    private:
+        glm::vec3 min_;
+        glm::vec3 max_;
+};
+#endif
+
+*/
+
