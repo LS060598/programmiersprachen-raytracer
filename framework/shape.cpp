374aa6ddd505484acf4de41fab390f97cabe53d0
LS060598 <laura.simon@uni-weimar.de>
2017-06-15 22:40:04 +0200

    5.3

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -1,26 +1,40 @@
 #include "shape.hpp"
 #include <cmath>
 #include <iostream>
-/*
+
+//5.3
 //Konstruktoren
 Shape::Shape():
 	name_{""},
-    color_{0.0,0.0,0.0,},
+    color_{0.0,0.0,0.0,} {}
 
 Shape::Shape(std::string const&  name, Color const& color):
 	name_{name},
-    color_{color},
+    color_{color} {}
+
+Shape::~Shape(){}
 
 
 
 //getter
-Color const& Shape::getColor() const{
-
+Color Shape::getColor() const{
 	return color_;
 }
 
-std::string const& Shape::getName() const{
-
+std::string Shape::getName() const{
 	return name_;
+}
+
+/*
+//5.4
+//Ausgabe von Objekten des Typs Shape
+std::ostream & Shape::print(std::ostream & os) const{
+	os << name_ << std::endl << color_ ; 
+	return os; //gibt Name & Farbe zurueck
+}
+
+// Überladen des Stream-Operators <<
+std::ostream & operator <<(std::ostream & os, Shape const& s){
+	return s.print(os);
 }
 */
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -1,22 +1,34 @@
 #ifndef BUW_SHAPE_HPP
 #define BUW_SHAPE_HPP
+#include <string>
+#include "color.hpp"
+#include <glm/vec3.hpp>
 
 class Shape{
     
     public:
-    /*
-        Color const& getcolor() const;
-        std::string const& getname() const;    
-        */    
+        //5.2
+        Shape();
+        Shape(std::string const& name, Color const& color);
+        ~Shape();
+    
+        Color getColor() const;
+        std::string getName() const; 
+
+         //5.1  
         virtual float area() const = 0; 
         virtual float volume() const = 0;
+/*
+        virtual std::ostream & print (std::ostream & os) const;
+        */
+        
 
 
 
-
-    private: /*
+    private: 
         Color color_;
         std::string name_;
-*/
-};
+
+};/*
+std::ostream & operator <<(std::ostream & os, Shape const& s);// oder vor endif???*/
 #endif
