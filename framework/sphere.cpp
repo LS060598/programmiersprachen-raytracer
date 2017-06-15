b7f932c143e229c0a9c7ba4068ba7d18a9579a40
LS060598 <laura.simon@uni-weimar.de>
2017-06-15 22:33:16 +0200

    5.2

--- framework/sphere.cpp
+++ framework/sphere.cpp
@@ -1,37 +1,69 @@
-/*
-#include "sphere.hpp"
-#include <glm/vec3.hpp>
-#include <cmath>
-#include <iostream>
 
-//destructor
+#include <iostream>
+#include "sphere.hpp"
+#include <cmath>
+//5.2
+//Constructors
 Sphere::Sphere():
 	Shape(),
-	center_{0.0,0.0,0.0},
-	radius_{0.0},
+	center_{0.0, 0.0, 0.0}, 
+	radius_{0.0} {}
+
+Sphere::Sphere(glm::vec3 const& center, float radius):
+	Shape(), 
+	center_{center}, 
+	radius_{radius} {}
+/*
+Sphere::Sphere(std::string const& name, Color const& color, glm::vec3 const& center, float const& radius):
+	Shape(name,color),	
+	center_{center},
+	radius_{radius}{}
+*/
+Sphere::~Sphere(){}
+
 
 //getter
-glm::vec3 Sphere::getCenter() const{
+glm::vec3 const& Sphere::getCenter() const{
 
 	return center_;
 }
 
-float Sphere::getRadius() const{
+float const& Sphere::getRadius() const{
 
 	return radius_;
 }
 
 //Methoden
 //area
-float Sphere::area() const{
-
-	return 4*M_PI*radius_*radius_;
-}
-
+float Sphere::area() const
+{
+		float pi = M_PI;
+		return (4 * pi * pow(radius_, 2));
+	}
 
 //volume
-float Sphere::volume() const{
+float Sphere::volume() const
+{
+		float pi = M_PI;
+		return ((4/3) * pi * pow(radius_, 3));
+}
+/*
+//5.5
+//überschreiben der Klasse print
+std::ostream & Sphere::print(std::ostream & os) const{
 
-	return (4/3)*M_PI*radius_*radius_*radius_;
+	Shape::print(os);
+	os<<"Center "		// glm::to_string(glm::vec3)
+	<<center_.x<<", "
+	<<center_.y<<", "
+	<<center_.z<<"  Radius: "
+	<<radius_<< ".";
+}
+
+//5.6
+//intersect
+bool Sphere::intersect(Ray const& ray, float& distance){
+	//ray._direction = glm::normalize(ray._direction);
+return glm::intersectRaySphere(ray.origin, ray.direction, center_, radius_ * radius_, distance);
 }
 */
--- framework/sphere.hpp
+++ framework/sphere.hpp
@@ -1,25 +1,34 @@
-/*
-#ifndef BUW_SPHERE_HPP
-#define BUW_SPHERE_HPP
+#ifndef SPHERE_HPP
+#define SPHERE_HPP
+
+#include <iostream>
 #include "shape.hpp"
+#include <string>
+#include <cmath>
 #include <glm/vec3.hpp>
+#include "ray.hpp"
 
-
-class Sphere : public Shape{
-
-
-    public:
-        glm::vec3 getCenter() const;
-	    float getRadius() const;
-	    double area() const override;
-        double volume() const override;
-
-
-
-
-    private:
-    glm::vec3 center_;
-    float rad_;
-};
-#endif
+class Sphere: public Shape{
+public:
+	Sphere();
+	Sphere(glm::vec3 const& center, float radius);
+	/*
+	Sphere(std::string const& name, Color const& color, glm::vec3 const& center, float const& radius);
+	*/
+	~Sphere();
+	glm::vec3 const& getCenter() const;
+	float const& getRadius() const;
+	float area() const /*override*/;
+	float volume() const /*override*/;
+	/*
+	std::ostream & print (std::ostream & os) const override;
+	//bool intersect const&(glm::vec3 const& origin, glm::vec3 const&direction);
+	bool intersect (Ray const& ray, float& distance);
 */
+private:
+	glm::vec3 center_;
+	float radius_;
+
+};
+
+#endif
