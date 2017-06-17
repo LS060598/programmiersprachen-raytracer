a50b551a4eb9e8793f0bac84f862952f0e557065
LS060598 <laura.simon@uni-weimar.de>
2017-06-17 14:39:18 +0200

    5.6

---
+++ framework/ray.hpp
@@ -0,0 +1,16 @@
+#ifndef BUW_RAY_HPP
+#define BUW_RAY_HPP
+#include <glm/vec3.hpp>
+
+struct Ray{
+
+	Ray();
+	Ray(glm::vec3 const& ori, glm::vec3 const& dir):
+
+	origin{ori},
+	direction{dir}{}
+
+	glm::vec3 origin;
+	glm::vec3 direction;
+};
+#endif
