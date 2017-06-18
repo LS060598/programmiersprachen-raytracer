5c5f9f8ce17a8b2fc4f75c952fb21aff4a9ed675
LS060598 <laura.simon@uni-weimar.de>
2017-06-18 15:19:01 +0200

    tut was es soll

--- framework/ray.hpp
+++ framework/ray.hpp
@@ -1,25 +1,32 @@
-#ifndef BUW_RAY_HPP
-#define BUW_RAY_HPP
+#ifndef RAY_HPP
+#define RAY_HPP
+
 #include <glm/vec3.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtx/intersect.hpp>
+
 
 struct Ray{
 
-	Ray();
-	Ray(glm::vec3 const& ori, glm::vec3 const& dir):
+  Ray():
+    origin_{},
+    direction_{}{}
 
-	origin{ori},
-	direction{dir}{}
+  //normalized direction in constructor!
+  Ray(glm::vec3 const& origin, glm::vec3 const& direction):
+    origin_{origin},
+    direction_{glm::normalize(direction)}{}
 
-	glm::vec3 const& getOrigin() const{
-    return origin;
-  	}
+  glm::vec3 const& getOrigin() const{
+    return origin_;
+  }
 
-  	glm::vec3 const& getDirection() const{
-    return direction;
-  	}
+  glm::vec3 const& getDirection() const{
+    return direction_;
+  }
 
+  glm::vec3 origin_;
+  glm::vec3 direction_;
 
-	glm::vec3 origin;
-	glm::vec3 direction;
 };
 #endif
